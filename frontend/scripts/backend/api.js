0f7c3cdfa5d728dcd8dac6432a066810f4fcac27
Haowen Rong <Alexrong1116@gmail.com>
2023-08-24 18:27:11 +0100

    Created initial function to save nodes

--- frontend/scripts/backend/api.js
+++ frontend/scripts/backend/api.js
@@ -62,3 +62,23 @@ export async function createUser(docData) {
     console.error('Error retrieving posts:', error);
   }
 }
+
+
+export async function saveNode(userId, node) {
+  try {
+    const response = await fetch(`http://localhost:3000/docs/${userId}/save`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(node)
+    });
+    if (!response.ok) {
+      console.log("Error creating doc");
+      return;
+    }
+    console.log("Creation was successful");
+  } catch (error) {
+    console.log('Error saving node', error);
+  }
+}
