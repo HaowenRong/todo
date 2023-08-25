ba32927cbaceef1c8e84fba34a942247ad2d9a39
HaowenRong <Alexrong1116@gmail.com>
2023-08-25 22:31:37 +0100

    Created function for retrieving nodes and base function for creating nodes

--- backend/controllers/postController.mjs
+++ backend/controllers/postController.mjs
@@ -1,17 +1,21 @@
-import { UserDoc } from '../models/post.mjs';
+import { Users } from '../models/post.mjs';
 
 export const getDocs = async (req, res) => {
   try {
-    const docs = await UserDoc.find({});
+    const docs = await Users.find({});
+    console.log(2);
     res.json(docs);
   } catch(error) {
+    console.log(error);
     res.status(500).json({ error: 'Error retreiving documents1'});
   }
 };
 
 export const getDocById = async (req, res) => {
   try {
-    const doc = await UserDoc.findById(req.params.id);
+    console.log(11);
+    const doc = await Users.findOne({ _id: req.params.id });
+    console.log(doc);
     res.json(doc);
   } catch(error) {
     res.status(500).json({ error: 'Error retreiving documents2'});
@@ -21,11 +25,9 @@ export const getDocById = async (req, res) => {
 export const getPageById = async (req, res) => {
   const { id, name } = req.params;
   console.log(req.params);
-  console.log(id);
-  console.log(name);
 
   try {
-    const doc = await UserDoc.findOne(
+    const doc = await Users.findOne(
       { _id: id, 'pages.title': name },
       { 'pages.$': 1 } // retrieve first matched page only
     ).populate('pages.nodes');
@@ -34,6 +36,38 @@ export const getPageById = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Error retrieving documents' });
+  }
+};
+
+
+export const getNodeById = async (req, res) => {
+  const { userId, pageId, nodeId } = req.params;
+
+  try {
+    // check parameters are not empty
+    if (!userId || !pageId || !nodeId) {
+      return res.status(400).json({ error: `Missing parameters. ${req.params}` });
+    }
+
+    const user = await Users.findOne({ _id: userId });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found.' });
+    }
+
+    const page = user.pages.find((page) => page.title === pageId);
+    if (!page) {
+      return res.status(404).json({ error: 'Page not found.' });
+    }
+
+    const node = page.nodes.find((node) => node._id.toString() === nodeId);
+    if (!node) {
+      return res.status(404).json({ error: 'Node not found.' });
+    }
+
+    res.json({ node });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error retrieving documents.' });
   }
 };
 
@@ -73,8 +107,21 @@ export const createDoc = async (req, res) => {
   }
 }
 
-export const saveNewNode = async (req, res) => {
+export const createNewNode = async (req, res) => {
+  try {
+    console.log(req.params);
 
+    // check if user exists
+    const doc = await UserDoc.findById(req.params.userId);
+    console.log(doc);
+    if (doc == null) { return; }
+
+    const node = await Nodes.findById(req.params.nodeId);
+    console.log(node);
+    res.json(doc);
+  } catch(error) {
+    res.status(500).json({ error: 'Error retreiving documents5'});
+  }
 }
 
 export const updateNode = async (req, res) => {
