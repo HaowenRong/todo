43d3824941ca846aa272ddfd90fc9e8f88d8e77a
HaowenRong <Alexrong1116@gmail.com>
2023-10-02 13:59:57 +0100

    Created models for pages and users

---
+++ backend/models/pages.mjs
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const PageSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'ListingsSchema',
+    default: null,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  modifiedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const Page = mongoose.model('tests', PageSchema);
---
+++ backend/models/users.mjs
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  modifiedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const User = mongoose.model('tests', UserSchema);
